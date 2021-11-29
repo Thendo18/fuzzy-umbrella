c210d8c764898ba48d988a994c61179178663b71
Thendo <thendomakherana@gmail.com>
2021-11-29 16:23:40 +0200

    changes

--- angular-todo-app/src/app/todos/todos.component.ts
+++ angular-todo-app/src/app/todos/todos.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
-
+todos: object[] | undefined;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Clean the house',
+        'completed': true,
+        'editing': false
+      },
+      {   'id': 2,
+        'title': 'Wash the car',
+        'completed': false,
+        'editing': false
+      },
+      {   'id': 3,
+        'title': 'book your next appointment',
+        'completed': false,
+        'editing': false
+      }
+    ];
+  }
   }
 
-}
+
